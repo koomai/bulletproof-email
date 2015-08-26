82b6b0ccd0838c273da5941729f15bb700b34a76
Sid K <sid@koomai.net>
2015-08-26 11:58:46 +1000

    glob sass files

--- gulp.config.js
+++ gulp.config.js
@@ -3,23 +3,23 @@
 */
 
 module.exports = function() {
-    var config = {
-        localDir: 'dist/local',
-        productionDir: 'dist/production',
-        localFiles: function() {
-            return [this.localDir + '/css/*.css', this.localDir + '/images/**/*', this.localDir + '/*.html']
-        },
-        sourcePath: {
-            sass: 'source/stylesheets/*.scss',
-            html: 'source/**/*.html',
-            layouts: 'source/layouts/*.html',
-            images: 'source/images/**/*'
-        },
-        browsersync: {
-            port: 8080,
-            open: false,
-            notify: true
-        }
-    };
-    return config;
+  var config = {
+    localDir: 'dist/local',
+    productionDir: 'dist/production',
+    localFiles: function() {
+      return [this.localDir + '/css/*.css', this.localDir + '/images/**/*', this.localDir + '/*.html']
+    },
+    sourcePath: {
+      sass: 'source/stylesheets/**/*.scss',
+      html: 'source/**/*.html',
+      layouts: 'source/layouts/*.html',
+      images: 'source/images/**/*'
+    },
+    browsersync: {
+      port: 8080,
+      open: false,
+      notify: true
+    }
+  };
+  return config;
 }
